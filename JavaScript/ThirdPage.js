cd780a79ab6627a7ec6180a64e29ce7ecd6ce86c
Sairaja99 <145500292+Sairaja99@users.noreply.github.com>
2024-05-29 19:20:03 +0530

    ThirdPage.js

--- JavaScript/ThirdPage.js
+++ JavaScript/ThirdPage.js
@@ -234,8 +234,8 @@ closeCart.onclick = () => {
 // Toggle Mode
 const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
-    document.body.classList.toggle("light-mode");
     document.body.classList.toggle("dark-mode");
+    document.body.classList.toggle("light-mode");
 })
 
 // Load Mode Preference
